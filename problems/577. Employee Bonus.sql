04d94f01d13c655a17025f15f78f884e3013c16e
Alexey Khoroshilov <lexakhoroshil@gmail.com>
2026-02-08 20:07:02 +0300

    20260208

---
+++ problems/577. Employee Bonus.sql
@@ -0,0 +1,6 @@
+/* Write your PL/SQL query statement below */
+select 
+    Employee.name,
+    Bonus.bonus
+from Employee left join Bonus on Employee.empId = Bonus.empId
+where Bonus.bonus <1000 or Bonus.bonus is NULL

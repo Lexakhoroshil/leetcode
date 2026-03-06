5c3eefa7365a729dbf975caa24282e0f15ad0312
Alexey Khoroshilov <lexakhoroshil@gmail.com>
2026-03-06 22:02:06 +0300

    20260306

---
+++ problems/1581. Customer Who Visited but Did Not Make Any Transactions.sql
@@ -0,0 +1,2 @@
+-- Write your PostgreSQL query statement below
+select Visits.customer_id, count(*) as count_no_trans from Visits where Visits.visit_id not in (select visit_id from Transactions) group by 1
